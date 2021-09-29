42743d615a9b4ea62809a466cc78a9a330bd2274
Kelvin <kelvin03pm@gmail.com>
2021-09-29 12:06:21 -0500

    working on layaout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav/nav-bar/nav-bar.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainLayaoutComponent } from './layout/main-layaout/main-layaout.component';
+import { FooterComponent } from './layout/nav/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    MainLayaoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/main-layaout/main-layaout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layaout',
+  templateUrl: './main-layaout.component.html',
+  styleUrls: ['./main-layaout.component.css']
+})
+export class MainLayaoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
